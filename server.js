6dee9df3972a214b65229b3b44e177218bd922d2
twhite <twhite17t@gmail.com>
2019-09-30 23:15:06 +1000

    did some simple database query test

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 // This is the main server file
 
 const express = require("express");
-
 const path = require("path");
+const mongoose = require("mongoose");
 
 const routes = require("./routes/main.json");
 
 require('dotenv').config();
 
+// This is the only schema that needs to be stored in the database for now
+const postSchema = mongoose.Schema({
+        title : String,
+        content : String,
+        date : {type : Date, default: Date.now},
+        visible : {type : Boolean, default: false},
+        tags : {type : Array, default: []}
+    }); 
+
+const BlogPost = mongoose.model("BlogPost", postSchema);
+
+
+/*
+    Connecting to mongodb and handling the case when the connection fails.
+    The address and database name are specfied by the MONGODB amd DBUSE flags in .env respectively
+*/
+mongoose.connect(process.env.MONGODB + "/" + process.env.DBUSE)
+    .then(() => console.log(`Connected to database ${process.env.DBUSE} @ ${process.env.MONGODB}`))
+    .catch(err => console.error(`Couldn't connect to ${process.env.MONGODB}`, err));
 
 
 const app = express();
-const port = process.env.DEVPORT;
+const port = process.env.DEVPORT; // the port that this server will run on
 
 app.use(express.json());
 
@@ -33,6 +52,18 @@ app.get("/", (req,res) => {
 app.get("/bundle.js", (req, res) => {
     res.sendFile(path.resolve(__dirname, "app", "dist", "bundle.js"));
 });
+
+const blogPost = new BlogPost({
+    title: "First Post Test",
+    content: "This is just a test, hopefully no-one actually sees this.",
+    visible: true
+})
+
+const test1 = async () => {
+    return await blogPost.save();
+}
+
+test1().then(console.log);
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
